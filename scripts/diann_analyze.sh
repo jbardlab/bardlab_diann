0236c2d82d4f6752a05df0bd354d4917a298d1d9
Jared Bard <jabard89@gmail.com>
2025-08-20 15:32:55 -0500

    removed some redundant files

--- scripts/diann_analyze.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-image_path="/scratch/group/jbardlab/containers/diann_docker:v0.1.sif"
-data_path="/scratch/user/jbard/Datasets/Pan_2024"
-
-singularity exec \
-    "${image_path}"  \
-    /bin/bash -c "/diann-2.2.0/diann-linux \
-    -h"
