482f34a974af6d74b16cca33c365828a65cdd1e3
Jared Bard <jabard89@gmail.com>
2025-08-20 21:20:34 -0500

    fixed run_diann and descriptions in chikv

--- scripts/run_diann_template.sh
+++ scripts/run_diann_template.sh
@@ -11,7 +11,7 @@ out_name="Zhou2024_diann"
     --fasta "${fasta_dir}/camprotR_240512_cRAP_20190401_full_tags.fasta" --cont-quant-exclude cRAP- \
     --gen-spec-lib --predictor --fasta-search \
     --threads 24 \
-    --out-lib "${out_dir}/${out_name}.predicted.speclib" \
+    --out-lib "${out_dir}/${out_name}.parquet" \
     --cut K*,R* \
     --missed-cleavages 1 \
     --var-mods 3 \
@@ -35,7 +35,7 @@ out_name="Zhou2024_diann"
     --verbose 4
 
 # then run the analysis
-    /diann-2.2.0/diann-linux \
+/diann-2.2.0/diann-linux \
     --dir "${data_dir}" \
     --fasta "${fasta_dir}/UP000005640_9606.fasta" \
     --fasta "${fasta_dir}/CHIKV_AF15561.fasta" \
@@ -49,7 +49,7 @@ out_name="Zhou2024_diann"
     --met-excision \
     --var-mod UniMod:35,15.994915,M \
     --var-mod UniMod:1,42.010565,*n \
-    --fixed-mod UniMod:4,57.`021464,C \
+    --fixed-mod UniMod:4,57.021464,C \
     --min-pep-len 7 \
     --max-pep-len 30 \
     --min-pr-charge 1 \
